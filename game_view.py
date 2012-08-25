68852af1f42269986fa5bc14d71396f0574c588b
Tom Gooding <tom.pudding@gmail.com>
2012-08-25 12:40:02 +0100

    Some basic physics

--- game_view.py
+++ game_view.py
@@ -63,12 +63,12 @@ class Intro(object):
     def __init__(self,parent):
         self.stage  = IntroStages.STARTED
         self.parent = parent
-        bl = Point(5,1)
+        bl = Point(0.5,0.5)
         self.letter_duration = 20
         self.start = None
         self.menu_text = ui.TextBox(parent   = parent,
                                     bl       = bl,
-                                    tr       = bl + Point(1,1),
+                                    tr       = bl + Point(0.1,0.5),
                                     text     = self.text,
                                     textType = drawing.texture.TextTypes.WORLD_RELATIVE,
                                     scale    = 3)
@@ -100,7 +100,8 @@ class Intro(object):
         self.elapsed = t - self.start
         self.stage = self.handlers[self.stage](t)
         if self.stage == IntroStages.COMPLETE:
-            self.parent.mode = GameMode(self.parent)
+            #self.parent.mode = GameMode(self.parent)
+            self.parent.mode = None
 
     def Startup(self,t):
         self.menu_text.EnableChars(0)
@@ -123,26 +124,76 @@ class Intro(object):
         else:
             return IntroStages.SCROLL
 
+class StaticBox(object):
+    def __init__(self,physics,bl,tr):
+        self.quad = drawing.Quad(globals.quad_buffer,tc = drawing.constants.full_tc)
+        self.physics = physics
+        self.bodydef = box2d.b2BodyDef()
+        midpoint = (tr - bl)*0.5*physics.scale_factor
+        self.bodydef.position = tuple((bl*physics.scale_factor) + midpoint)
+        self.shape = box2d.b2PolygonDef()
+        self.shape.SetAsBox(*midpoint)
+        self.body = physics.world.CreateBody(self.bodydef)
+        self.shape.density = 1
+        self.shape.friction = 0.3
+        self.body.CreateShape(self.shape)
+
+class DynamicBox(StaticBox):
+    def __init__(self,physics,bl,tr):
+        super(DynamicBox,self).__init__(physics,bl,tr)
+        self.body.SetMassFromShapes()
+        physics.AddObject(self)
+
+    def Update(self):
+        #Just set the vertices
+        bl = (Point(*self.shape.vertices[0]) + Point(*self.body.position))/self.physics.scale_factor
+        tr = (Point(*self.shape.vertices[2]) + Point(*self.body.position))/self.physics.scale_factor
+        #print bl,tr
+        #print self.body.position
+        self.quad.SetVertices(bl,tr,10)
+        
+
 class Physics(object):
-    def __init__(self):
+    scale_factor = 0.1
+    def __init__(self,parent):
+        self.parent = parent
         self.worldAABB=box2d.b2AABB()
-        self.worldAABB.lowerBound.Set(-100, -100)
-        self.worldAABB.upperBound.Set(100, 100)
+        self.worldAABB.lowerBound = (-100,-100)
+        self.worldAABB.upperBound = (100 + self.parent.absolute.size.x*self.scale_factor,100 + self.parent.absolute.size.y*self.scale_factor + 100)
+        self.gravity = (0,-10)
+        self.doSleep = True
+        self.world = box2d.b2World(self.worldAABB, self.gravity, self.doSleep)
+        self.timeStep = 1.0 / 60.0
+        self.velocityIterations = 10
+        self.positionIterations = 8
+        self.objects = []
+    
+    def AddObject(self,obj):
+        self.objects.append(obj)
 
-
+    def Step(self):
+        self.world.Step(self.timeStep, self.velocityIterations, self.positionIterations)
+        for obj in self.objects:
+            obj.Update()
 
 class GameView(ui.RootElement):
     def __init__(self):
-        super(GameView,self).__init__(Point(0,0),globals.screen)
+        super(GameView,self).__init__(Point(0,0),Point(globals.screen.x*10,globals.screen.y*2))
         self.texture = drawing.texture.Texture('starfield.png')
         self.uielements = {}
         self.backdrop   = drawing.Quad(globals.quad_buffer,tc = numpy.array([(0,0),(0,1),(5,1),(5,0)]))
         self.backdrop.SetVertices(Point(0,0),
-                                  Point(globals.screen.x*10,globals.screen.y*2),
+                                  self.absolute.size,
                                   drawing.constants.DrawLevels.grid)
         self.viewpos = Viewpos(Point(globals.screen.x*5,globals.screen.y))
         self.t = None
-        self.physics = Physics()
+        self.physics = Physics(self)
+        self.floor = StaticBox(self.physics,
+                               bl = Point(0,0),
+                               tr = Point(self.absolute.size.x,50))
+        self.box   = DynamicBox(self.physics,
+                                bl = Point(self.absolute.size.x*0.55,1200),
+                                tr = Point(self.absolute.size.x*0.55+50,1250))
         
         self.mode = Intro(self)
 
@@ -187,13 +238,19 @@ class GameView(ui.RootElement):
         self.mode.KeyDown(key)
 
     def MouseButtonDown(self,pos,button):
-        return self.mode.MouseButtonDown(pos,button)
+        print self.viewpos.pos + pos
+        if self.mode:
+            return self.mode.MouseButtonDown(pos,button)
+        else:
+            return False,False
 
     def Update(self,t):
         if self.t == None:
             self.t = t
+        self.physics.Step()
 
         self.viewpos.Update(t)
-        self.mode.Update(t)
+        if self.mode:
+            self.mode.Update(t)
 
         self.Draw()
